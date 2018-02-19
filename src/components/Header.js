692de2c4b2fcdc82bf4326edc7510d740b714130
HaoZeke <rohit.goswami@aol.com>
2018-02-20 00:42:46 +0530

    Initialize content [committee] and the countdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import Countdown from 'react-count-down'
+import React, { Component, PropTypes } from 'react'
+
+const cb = () => {
+  console.log('expired callback')
+}
+
+const OPTIONS = { endDate: '03/09/2018 10:55 AM', prefix: 'Left', cb}
+
+const EventCountdown = () => (
+  <Countdown options={OPTIONS} />
+)
+
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
@@ -7,16 +19,17 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>HBTU MUN 2018</h1>
+                <h3> <EventCountdown /> </h3>
+                <p>The <a href="https://hbtumun17.netlify.com">second iteration</a> of the <a href="http://hbtu.ac.in/">Harcourt Butler Technical University</a> Model United Nations. Held in tandem with <a href="www.confluencehbti2k18.wixsite.com/hbtu">Confluence</a>.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('committee')}}>Committee</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('agenda')}}>Agenda</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('eb')}}>Executive Board</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('reg')}}>Registration</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Link from 'gatsby-link'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import picdisec from '../images/xlargeDISEC.png'
+import picunga from '../images/disecRoom.jpg'
 
 class Main extends React.Component {
   render() {
@@ -13,24 +15,53 @@ class Main extends React.Component {
     return (
       <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <article id="committee" className={`${this.props.article === 'committee' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">GA-DISEC</h2>
+          <span className="image main"><img src={picdisec} alt="" /></span>
+          <h3>General Assembly</h3>
+          <p>At the center of the UN system is the 193-member General Assembly (GA), comprised of seven
+            main committees and various subsidiary and related bodies. The GA serves primarily as a forum
+            for discussing general issues such as international peace and security and international
+            collaboration in economic, social, cultural, educational, and health fields. The GA is also able to
+            establish committees and other bodies to study and report on specific issues. Although the
+            decisions of the GA have no binding legal force upon member-states, they do carry the weight of
+            the moral authority of the world community.
+          </p>
+          <h3>DISEC [First Committee]</h3>
+          <span className="image main"><img src={picunga} alt="" /></span>
+          <h4>History</h4>
+            <p>Established to deal with the technological implications of the atomic bomb used against Japan and in
+              development by every other major military power of the day, the General Assembly First Committee of
+              1946 focused on all political and security questions of the General Assembly’s agenda. Along with the
+              then-unnumbered Special Political Committee, the First Committee provided the stage for the liveliest
+              debates between superpowers during the Cold War, centering on armament stockpiling and territorial
+              balance between Soviet communism and Western capitalism.</p>
+          <h4>Mandate</h4>
+            <p>The First Committee deals with disarmament, global challenges and threats to peace that affect the international community and seeks out solutions to the challenges in the international security regime.</p>
+            <p>It considers all disarmament and international security matters within the scope of the Charter or relating to the powers and functions of any other organ of the United Nations; the general principles of cooperation in the maintenance of international peace and security, as well as principles governing disarmament and the regulation of armaments; promotion of cooperative arrangements and measures aimed at strengthening stability through lower levels of armaments. </p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
+        <article id="agenda" className={`${this.props.article === 'agenda' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Agenda</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+          <p>asdsrcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+          <span className="image main"><img src={pic02} alt="" /></span>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
+        <article id="eb" className={`${this.props.article === 'eb' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Executive Board</h2>
+          <span className="image main"><img src={pic02} alt="" /></span>
+          <p>asdsrcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+          <span className="image main"><img src={pic02} alt="" /></span>
+          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          {close}
+        </article>
+
+        <article id="reg" className={`${this.props.article === 'reg' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Registration</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
           <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
           {close}
@@ -57,10 +88,8 @@ class Main extends React.Component {
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="#" className="icon fa-trello"><span className="label">Trello</span></a></li>
           </ul>
           {close}
         </article>
